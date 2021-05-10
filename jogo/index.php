73c3b3d998dcfb231dd2c5b738d3273d65718c4c
Clésar Sapelli Junior <clesarsapellijunior@gmail.com>
2021-05-10 18:09:18 -0300

    var

--- jogo/index.php
+++ jogo/index.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    Jogo dados
-</body>
-</html>
+<?php
+    $pessoa = (isset($_POST['pessoa']) ? $_POST['pessoa'] : '');
+    $maquina = "Máquina";
+
+    $pessoaDado1 = 0;
+    $pessoaDado2 = 0;
+    $maquinaDado1 = 0;
+    $maquinaDado2 = 0;
+
+    $somaPessoa = 0;
+    $somaMaquina = 0;
+?>
